b76568fbcbdcff5b399594aea465feb1b697d185
kangsudal <kangsudal@gmail.com>
2018-11-01 01:50:37 +0900

    Adding Tag by taggit

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):#포스팅에 저장될 공간
@@ -8,6 +9,7 @@ class Post(models.Model):#포스팅에 저장될 공간
 	publishedDate = models.DateTimeField(blank=True, null=True)
 	modifiedDate = models.DateTimeField(blank=True, null=True)
 	contents = models.TextField()
+	tag = TaggableManager(blank=True)
 
 	def __str__(self):
 		return self.postname

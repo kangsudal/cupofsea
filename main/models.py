cd89fdf384b324afce10cbbe0ba1939876c1a6d8
kangsudal <kangsudal@gmail.com>
2018-11-01 00:09:33 +0900

    https://stackoverflow.com/questions/51568408/when-uploading-an-image-in-admin-get-error-file-extension-jpg-is-not-allowed

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class Post(models.Model):#포스팅에 저장될 공간
 	postname = models.CharField(max_length=50)
-	mainphoto = models.ImageField(blank=True, null=True)
+	mainphoto = models.ImageField(blank=True, null=True, validators=[FileExtensionValidator(['jpg'])])
 	contents = models.TextField()
 
 	def __str__(self):
